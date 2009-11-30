ea29082e9a6a1dc52179afd63d5b14261e9b82e4
Jeff Hui <contrib@jeffhui.net>
2009-11-30 12:53:01 -0500

    Added inflection support and unit tests for utils.clj. Pluralizzations and singularizations are now possible.

---
+++ src/hui/blog/inflection.clj
@@ -0,0 +1,179 @@
+(ns hui.blog.inflection
+  "Enables english-based inflection capabilities such as pluralization."
+  (:use hui.blog.utils))
+
+;; words that can't be pluralized: eg - money
+(def uncountable-words (ref #{}))
+;; this is like (seq {}) form, that is: ((key val) (key val) .. etc)
+;; maps singular => plural
+(def plural-mapping (ref '()))
+;; maps plural => singular
+(def singular-mapping (ref '()))
+
+(defn add-uncountable-inflection
+  "Adds a sequence of words to the immutable pluralization/singularization
+  list. These words do not have plural:singular mappings. Eg - money."
+  [& words] (dosync (alter uncountable-words into words)))
+(defn remove-uncountable-inflection
+  "Removes a sequence of words from the uncountable list; most likely
+  because the word was moved to mapping sequence."
+  [& words] (dosync (ref-set uncountable-words
+			     (remove (set words) @uncountable-words))))
+
+(defn- convert-to-regex
+  "Converts vectors and strings into patterns for inflection functions."
+  [value] (cond (vector? value) (apply re-compile value)
+		(string? value) (re-compile "(?i)" value "$")
+		true value))
+
+(defn- inflection-args-valid
+  "Validates there are even number of args greater than 1. Used for
+  inflection functions."
+  [x y pairs] (and x y (= (mod (count pairs) 2) 0)))
+
+(defn add-plural-inflection
+  "Adds a normal regex singular to plural mapping."
+  [singular-re plural-replace & more-pairs]
+  (if-not (inflection-args-valid singular-re plural-replace more-pairs)
+    (throw-arg "Invalid singular/plural pair(s)."))
+  (let [singular-re (convert-to-regex singular-re)
+	plural-replace (apply str plural-replace)]
+    (dosync (alter plural-mapping conj [singular-re plural-replace]))
+    (if more-pairs (let [[s p & r] more-pairs] (recur s p r)))))
+
+(defn add-singular-inflection
+  "Adds a normal regex plurla to singular mapping."
+  [plural-re singular-replace & more-pairs]
+  (if-not (inflection-args-valid plural-re singular-replace more-pairs)
+    (throw-arg "Invalid singular/plural pair(s)."))
+  (let [plural-re (convert-to-regex plural-re)
+	singular-replace (apply str singular-replace)]
+    (dosync (alter singular-mapping conj [plural-re singular-replace]))
+    (if more-pairs (let [[s p & r] more-pairs] (recur s p r)))))
+
+(defn add-irregular-inflection
+  "Adds an irregular mapping of plural:singular words. This is for words
+  whose pluralizations is abnormally different from normal rules."
+  [singular plural & more-pairs]
+  (if-not (inflection-args-valid singular plural more-pairs)
+    (throw-arg "Invalid singular/plural pair(s)."))
+  (dosync
+   (remove-uncountable-inflection singular plural)
+   (let [[fsing & rsing] (str singular)
+	 [fplur & rplur] (str plural)
+	 [fsing fplur] [(str fsing) (str fplur)]
+	 all (map #(apply str %) [fsing rsing fplur rplur])
+	 [fsing rsing fplur rplur] all]
+     (if (and (isUpperCase? fsing) (isUpperCase? fplur))
+       (do
+	 (add-plural-inflection
+	  ["(?i)(" fsing ")" rsing "$"] ["\1" rplur]
+	  ["(?i)(" fplur ")" rplur "$"] ["\1" rplur])
+	 (add-singular-inflection
+	  ["(?i)(" fplur ")" rplur "$"] ["\1" rsing]))
+       (let [upper-and-lower (fn [x] [(.toUpperCase x) (.toLowerCase x)])
+	     firsts (into (upper-and-lower fsing) (upper-and-lower fplur))
+	     [ufsing lfsing ufplur lfplur] firsts]
+	 (apply add-plural-inflection
+		(interleave
+		 (map (fn [x y] [x "(?i)" y])
+		      firsts [rsing rsing rplur rplur])
+		 (map (fn [x] [x rplur]) firsts)))
+	 (add-singular-inflection
+	  [ufplur "(?i)" rplur] [ufsing rsing]
+	  [lfplur "(?i)" rplur] [lfsing rsing])))))
+  (if more-pairs (let [[s p & r] more-pairs] (recur s p r))))
+
+(defn- create-mapping
+  "Generates a function that can be used for pluralizing or singularizing."
+  [map-ref]
+  (fn [word] 
+    (or (some #(and (.equalsIgnoreCase % word) word) @uncountable-words)
+	(loop [[rule repl] (first @map-ref)
+	       r (rest @map-ref)]
+	  (let [new (re-gsub rule repl word)]
+	    (if (> (count (re-partition rule word)) 1)
+	      (re-gsub rule repl word)
+	      (recur (first r) (rest r))))))))
+
+(defmacro defmapping
+  "Syntantic sugar for creating a pluralize/singular mapping."
+  {:private true}
+  [symbol m-ref doc & tests]
+  `(def #^{:doc ~doc :test (fn [] ~@tests)} ~symbol (create-mapping ~m-ref)))
+
+(defmapping pluralize plural-mapping
+  "Pluralizes a given word. Does nothing if the word is already plural."
+  (are [x y] (= x (pluralize y))
+       "pizzas" "pizza"
+       "matrices" "matrix"
+       "cakes" "cake"
+       "Oranges" "Orange"
+       "apples" "apples"
+       "money" "money"))
+
+(defmapping singularize singular-mapping
+  "Singulizes a given word. Does nothing if the word is already singular."
+  (are [x y] (= x (pluralize y))
+       "pizza" "pizzas"
+       "matrix" "matrices"
+       "cake" "cakes"
+       "Orange" "Oranges"
+       "apple" "apple"
+       "money" "money"))
+
+;;;;; pluralization / singularization knowledge base from RAILs ;;;;;
+(add-plural-inflection ;; "(?i) is prepended and $ is postfixed to RE
+ "" "s"
+ "s" "s"
+ "(ax|test)is" "$1es"
+ "(octop|vir)us" "$1i"
+ "(alias|status)" "$1es"
+ "(bu)s" "$1ses"
+ "(buffal|tomat)o" "$1oes"
+ "([ti])um" "$1a"
+ "sis" "ses"
+ "(?:([^f])fe|([lr])f)" "$1$2ves"
+ "(hive)" "$1s"
+ "([^aeiouy]|qu)y" "$1ies"
+ "(x|ch|ss|sh)" "$1es"
+ "(matr|vert|ind)(?:ix|ex)" "$1ices"
+ "([m|l])ouse" "$1ices"
+ "(quiz)" "$1zes")
+
+(add-singular-inflection
+ "s" ""
+ "(n)ews" "$1ews"
+ "([ti])a" "$1um"
+ "((a)naly|(b)a|(d)iango|(p)arenthe|(p)rongo|(s)ynop|(t)he)ses" "$1$2sis"
+ "(^analy)ses" "$1sis"
+ "([^f])ves" "$1fe"
+ "(hive)s" "$1"
+ "(tive)s" "$1"
+ "([lr])ves" "$1f"
+ "([^aeiouy]|qu)ies" "$1y"
+ "(s)eries" "$1eries"
+ "(m)ovies" "$1ovie"
+ "(x|ch|ss|sh)es" "$1"
+ "([m|l])ice" "$1ouse"
+ "(bus)es" "$1"
+ "(o)es" "$1"
+ "(shoe)s" "$1"
+ "(cris|ax|test)es" "$1"
+ "(octop|vir)i" "$1us"
+ "(alias|status)es" "$1"
+ "(ox)en" "$1"
+ "(vert|ind)ices" "$1ex"
+ "(matr)ices" "$1ix"
+ "(quiz)zes" "$1"
+ "(database)s" "$1")
+
+(add-irregular-inflection
+ "person" "people"
+ "man" "men"
+ "child" "children"
+ "move" "moves"
+ "cow" "kine")
+
+(add-uncountable-inflection
+ "equipment" "information" "rice" "money" "species" "series" "fish" "sheep")
--- src/hui/blog/utils.clj
+++ src/hui/blog/utils.clj
@@ -2,26 +2,68 @@
   "Provides utility functions. All functions in this namespace are pure."
   (:use compojure
 	[clojure.contrib.java-utils :only (as-str)]
-	[clojure.contrib.str-utils :only (re-split re-gsub)])
-  (:import [java.net URL]))
+	[clojure.contrib.str-utils :only (re-split re-partition)]
+	[clojure.contrib.except :only (throw-arg)]
+	clojure.test)
+  (:import java.net.URL
+	   java.util.regex.Pattern))
+
+(defn character?
+  "Returns true if the given value is a Character class."
+  [c] (instance? Character c))
+
+(defn re-compile
+  "Merges a series of parameters as a string and concats them together as a
+  single string for Pattern.compile."
+  [& strings] (Pattern/compile (apply str strings)))
+
+(defn re-quote
+  "Escapes a given regex string so that its contents are safe from regex
+  evaluation. This is good for escaping user input from injecting custom
+  regex in your own expressions."
+  [s] (Pattern/quote s))
+
+(defn re-safe
+  "Creates a Pattern instance where the given string is matched directly
+  without any regular expressions. Good for escaping user input from
+  injecting regular expressions."
+  [s] (Pattern/compile (re-quote s)))
 
 (defn merge-url
   "Merges relative url fragments. Ensures there are no duplicate slashes (/)
   with the exception of http[s]:// at the start of the string"
+  {:test (fn []
+	   (are [x y] (= x (apply merge-url y))
+		"http://google.com/"
+		["http://" "google.com"],
+		"https://google.com/search/"
+		["https://" "google.com" "/search"],
+		"localhost:80/wtf/ya/"
+		["localhost" ":80" "/wtf/" "ya"]))}
   [& fragments]
   (let [url-frags (filter #(not= % "")
-			  (re-split #"/" (reduce str fragments)))
+			  (re-split #"/" (reduce as-str fragments)))
 	str-url (reduce str (interleave url-frags (repeat "/")))]
     (re-gsub #"^([hH][tT]{2}[pP][sS]?):" "$1:/" str-url)))
-
-;(defn build-url
-;  [& partials]
-;  (in
 
 (defn absolute-url
   "Generates the root of the url for this incoming request or takes an
   absolute url without the server/domain and appends the information to
   it."
+  {:test (fn []
+	   (let [headers (fn [scheme name port]
+			   {:scheme scheme
+			    :server-name name
+			    :server-port port})
+		 h1 (headers "http" "localhost" 88)
+		 h2 (headers "https" "google.com" 80)
+		 h3 (headers "http" "domain.com" 80)]
+	     (are [x y] (= x (apply absolute-url y))
+		  "http://localhost:88/" [h1]
+		  "http://localhost:88/test/" [h1 :test]
+		  "http://localhost:88/test/1/" [h1 "test/" "1/"]
+		  "https://google.com/" [h2]
+		  "http://domain.com/" [h3])))}
   [request & relative-url-fragments]
   (apply merge-url
    (str (URL. (as-str (:scheme request))
@@ -29,3 +71,19 @@
 	      (if (= (:server-port request) 80) -1 (:server-port request))
 	      "/"))
    relative-url-fragments))
+
+(defn str-blank?
+  "Returns true if the string is empty or has only whitespace characters."
+  {:test (fn []
+	   (are [x y] (x (str-blank? y))
+		true? "", true? " \t\n", false? "hi"))}
+  [s]
+  (every? #(Character/isWhitespace %) s))
+
+(defn isUpperCase?
+  "Returns true if the string is in all uppercase."
+  {:test (fn []
+	   (are [x y] (x (isUpperCase? y))
+		true? "A", true? "HELLO",
+		false? "b", false? "falsy"))}
+  [s] (let [s (str s)] (= s (.toUpperCase s))))
